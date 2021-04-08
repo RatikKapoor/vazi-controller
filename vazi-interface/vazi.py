acbf66d4e95b3dc7a916f2478ed24e684a7ee2b0
Ratik Kapoor <ratikrk@gmail.com>
2021-04-07 22:08:45 -0600

    feat: Dynamic colours

--- vazi-interface/vazi.py
+++ vazi-interface/vazi.py
@@ -1,19 +1,31 @@
 from pynput.keyboard import Key, Controller
 import serial
+import threading
 from PIL import ImageGrab
-
-
-def rgbToDec(n: int) -> int:
-    return int(n / 255 * 99)
-
 
 keyboard = Controller()
 
-ser = serial.Serial('/dev/cu.usbserial-0001', 115200)
+ser = serial.Serial('/dev/cu.usbserial-0001', 115200, timeout=0.05)
 
-while True:
-    [action, button] = ser.readline().decode("utf-8").strip().split("-")
-    key = Key.up
+
+def rgbToDec(n: int) -> int:
+    return int(n / 255 * 30)  # FIXME: Change 30 -> 99
+
+
+def padRGB(rgba) -> bytes:
+    (r, g, b, a) = rgba
+    return str.encode(str(r).zfill(2)) + str.encode(
+        str(g).zfill(2)) + str.encode(str(b).zfill(2))
+
+
+def averageColours(image: list[int]):
+    print(image)
+
+
+def handleButton(line: str) -> None:
+    [action, button] = line.decode().strip().split("-")
+
+    key = None
     if button == 'a':
         key = 'a'
     if button == 'b':
@@ -33,24 +45,59 @@ while True:
 
     if action == "bp":
         keyboard.press(key)
-    else:
+
+    elif action == "br":
         keyboard.release(key)
 
-    if action == "bp" and key == Key.shift_r:
+
+def buttonThread() -> None:
+    while True:
+        line = ser.readline()
+        if line.__len__() > 0:
+            handleButton(line)
+
+
+def colourThread() -> None:
+    while True:
         screenshot = ImageGrab.grab()
 
-        pixel = screenshot.getpixel(
-            (screenshot.width / 4, screenshot.height / 4))
+        top_left = screenshot.crop([
+            screenshot.width * 1 / 8, screenshot.height * 1 / 8,
+            screenshot.width * 3 / 8, screenshot.height * 3 / 8
+        ])
+        top_right = screenshot.crop([
+            screenshot.width * 5 / 8, screenshot.height * 1 / 8,
+            screenshot.width * 7 / 8, screenshot.height * 3 / 8
+        ])
+        bottom_left = screenshot.crop([
+            screenshot.width * 1 / 8, screenshot.height * 5 / 8,
+            screenshot.width * 3 / 8, screenshot.height * 7 / 8
+        ])
+        bottom_right = screenshot.crop([
+            screenshot.width * 5 / 8, screenshot.height * 5 / 8,
+            screenshot.width * 7 / 8, screenshot.height * 7 / 8
+        ])
 
-        (r, g, b, a) = map(rgbToDec, pixel)
+        top_left = top_left.resize((1, 1)).getpixel((0, 0))
+        top_right = top_right.resize((1, 1)).getpixel((0, 0))
+        bottom_left = bottom_left.resize((1, 1)).getpixel((0, 0))
+        bottom_right = bottom_right.resize((1, 1)).getpixel((0, 0))
 
-        print(r, g, b)
+        tl = map(rgbToDec, top_left)
+        tr = map(rgbToDec, top_right)
+        bl = map(rgbToDec, bottom_left)
+        br = map(rgbToDec, bottom_right)
 
-        res = str.encode(str(r)) + str.encode(str(g)) + str.encode(str(b))
-        print(res * 4)
+        tl = padRGB(tl)
+        tr = padRGB(tr)
+        bl = padRGB(bl)
+        br = padRGB(br)
 
-        ser.write(res * 4)
+        ser.write(tr + tl + bl + br)
 
-# keyboard.press(Key.right)
-# time.sleep(1)
-# keyboard.release(Key.right)
+
+if __name__ == "__main__":
+    button = threading.Thread(target=buttonThread)
+    colour = threading.Thread(target=colourThread)
+    button.start()
+    colour.start()
