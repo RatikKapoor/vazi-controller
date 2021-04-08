7b2bb5801e7d96fd21d75a7f9ac868f92d1b948d
Ratik Kapoor <ratikrk@gmail.com>
2021-04-07 18:33:10 -0600

    feat: Python buttons and RGB

--- vazi-interface/vazi.py
+++ vazi-interface/vazi.py
@@ -1,6 +1,11 @@
 from pynput.keyboard import Key, Controller
 import serial
-import time
+from PIL import ImageGrab
+
+
+def rgbToDec(n: int) -> int:
+    return int(n / 255 * 99)
+
 
 keyboard = Controller()
 
@@ -8,8 +13,6 @@ ser = serial.Serial('/dev/cu.usbserial-0001', 115200)
 
 while True:
     [action, button] = ser.readline().decode("utf-8").strip().split("-")
-    # print(action)
-    # print(button)
     key = Key.up
     if button == 'a':
         key = 'a'
@@ -32,6 +35,21 @@ while True:
         keyboard.press(key)
     else:
         keyboard.release(key)
+
+    if action == "bp" and key == Key.shift_r:
+        screenshot = ImageGrab.grab()
+
+        pixel = screenshot.getpixel(
+            (screenshot.width / 4, screenshot.height / 4))
+
+        (r, g, b, a) = map(rgbToDec, pixel)
+
+        print(r, g, b)
+
+        res = str.encode(str(r)) + str.encode(str(g)) + str.encode(str(b))
+        print(res * 4)
+
+        ser.write(res * 4)
 
 # keyboard.press(Key.right)
 # time.sleep(1)
